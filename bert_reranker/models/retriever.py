b0fb7e100c9863c0d1f8250e14382a424b4967e4
Jeremy Pinto <jerpint@gmail.com>
2020-04-03 15:04:18 -0400

    Fix losses (#16)
    * Fix negative_sampling loss
    
    * Adapt triplet loss

--- bert_reranker/models/retriever.py
+++ bert_reranker/models/retriever.py
@@ -144,22 +144,38 @@ class RetrieverTrainer(pl.LightningModule):
         all_prob = torch.sigmoid(all_dots)
 
         if self.loss_type == 'negative_sampling':
-            raise ValueError('need to fix now that we have target that is not always 0')
-            pos_loss = - torch.log(all_prob[:, 0]).sum()
-            neg_loss = - torch.log(1 - all_prob[:, 1:]).sum()
+            pos_preds = []
+            neg_preds = all_prob.clone()
+            for count, target in enumerate(targets):
+                pos_preds.append(neg_preds[count][target].clone())
+                neg_preds[count][target] = 0
+            pos_preds = torch.tensor(pos_preds).to(q_emb.device)
+            pos_loss = - torch.log(pos_preds).sum()
+            neg_loss = - torch.log(1 - neg_preds).sum()
             loss = pos_loss + neg_loss
         elif self.loss_type == 'classification':
             # all_dots are the logits
             loss = self.cross_entropy(all_dots, targets)
         elif self.loss_type == 'triplet_loss':
-            raise ValueError('need to fix now that we have target that is not always 0')
-            assert p_embs.shape[1] == 3
-            # picking a random negative example
+
+            # draw random wrong targets
+            wrong_targs = []
+            for target in targets:
+                wrong_targ = list(range(0, p_embs.shape[1]))
+                wrong_targ.remove(target)
+                random.shuffle(wrong_targ)
+                wrong_targs.extend([wrong_targ[0]])
+
+
+            pos_pembs = torch.stack([p_embs[idx, target] for (idx, target) in enumerate(targets)])
+            neg_pembs = torch.stack([p_embs[idx, wrong_targ] for (idx, wrong_targ) in enumerate(wrong_targs)])
+
             negative_index = random.randint(1, 2)
             triplet_loss = nn.TripletMarginLoss(margin=1.0, p=2)
             loss = triplet_loss(q_emb,
-                                p_embs[:, 0, :],
-                                p_embs[:, negative_index, :])
+                                pos_pembs,
+                                neg_pembs,
+                                )
         elif self.loss_type == 'cosine':
             raise ValueError('need to fix now that we have target that is not always 0')
             targets = torch.ones(batch_size, num_document)
