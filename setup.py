afa6fe0f676c15ff504c995564a7f414fd45f093
mirkobronzi <m.bronzi@gmail.com>
2020-04-12 13:56:40 -0400

    and also setuptools...

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     python_requires='>=3.7',
     install_requires=[
         'flake8', 'tqdm', 'pyyaml>=5.3', 'pytest', 'numpy>=1.16.4', 'pytest',
-        'torch', 'transformers', 'pytorch-lightning', 'sklearn', 'pandas'],
+        'torch', 'transformers', 'pytorch-lightning', 'sklearn', 'pandas', 'setuptools>=41.0.0'],
     entry_points={
         'console_scripts': [
             'main=bert_reranker.main:main'
