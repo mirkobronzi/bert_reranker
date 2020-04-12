5439cb482258580a25d581fca4409bf489d4f3fa
mirkobronzi <m.bronzi@gmail.com>
2020-04-12 15:16:23 -0400

    fixed path

--- tests/end2end/run_exp1.sh
+++ tests/end2end/run_exp1.sh
@@ -2,6 +2,8 @@
 set -e
 # go to the test folder
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd ${DIR}/../../examples/local
+cd ${DIR}
 
 main --config config.yaml --output output --train
+main --config config.yaml --output output --validate
+main --config config.yaml --output output --predict ../../examples/local/dev.json
