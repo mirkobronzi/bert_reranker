6a6017203618e1fc63c1b47b7d482a04c281d89a
mirkobronzi <m.bronzi@gmail.com>
2020-04-12 15:07:32 -0400

    changed exp folder

--- tests/end2end/run_exp1.sh
+++ tests/end2end/run_exp1.sh
@@ -4,4 +4,4 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ${DIR}/../../examples/local
 
-main --config config.yaml --output output --no-model-restoring --train
+main --config config.yaml --output output --train
