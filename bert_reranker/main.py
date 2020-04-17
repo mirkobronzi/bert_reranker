91da0bec7903bf565273541845340d0871a0b486
mirkobronzi <m.bronzi@gmail.com>
2020-04-17 16:56:12 -0400

    refactored main

--- bert_reranker/main.py
+++ bert_reranker/main.py
@@ -59,90 +59,9 @@ def main():
     with open(args.config, 'r') as stream:
         hyper_params = load(stream, Loader=yaml.FullLoader)
 
-    check_and_log_hp(
-        ['train_file', 'dev_files', 'test_file', 'batch_size', 'tokenizer_name',
-         'model', 'max_question_len', 'max_paragraph_len', 'patience', 'gradient_clipping',
-         'max_epochs', 'loss_type', 'optimizer',  'precision', 'accumulate_grad_batches', 'seed'],
-        hyper_params)
-
-    if hyper_params['seed'] is not None:
-        # fix the seed
-        torch.manual_seed(hyper_params['seed'])
-        np.random.seed(hyper_params['seed'])
-        torch.backends.cudnn.deterministic = True
-        torch.backends.cudnn.benchmark = False
-
-    os.makedirs(hyper_params['cache_folder'], exist_ok=True)
-
-    tokenizer_name = hyper_params['tokenizer_name']
-    tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
-
-    train_dataloader = generate_dataloader(
-        hyper_params['train_file'], hyper_params['max_question_len'],
-        hyper_params['max_paragraph_len'], tokenizer, hyper_params['batch_size'],
-        num_workers=args.num_workers, shuffle=True)
-
-    dev_dataloaders = []
-    for dev_file in hyper_params['dev_files'].values():
-        dev_dataloaders.append(
-            generate_dataloader(
-                dev_file,
-                hyper_params['max_question_len'],
-                hyper_params['max_paragraph_len'],
-                tokenizer, hyper_params['batch_size'],
-                num_workers=args.num_workers,
-                shuffle=False
-            )
-        )
-
-    test_dataloader = generate_dataloader(
-        hyper_params['test_file'], hyper_params['max_question_len'],
-        hyper_params['max_paragraph_len'], tokenizer, hyper_params['batch_size'],
-        num_workers=args.num_workers, shuffle=False)
-
-    ret = load_model(hyper_params, tokenizer, args.debug)
-
-    os.makedirs(args.output, exist_ok=True)
-    checkpoint_callback = ModelCheckpoint(
-        filepath=os.path.join(args.output, '{epoch}-{val_acc_0:.2f}-{val_loss_0:.2f}'),
-        save_top_k=1,
-        verbose=True,
-        monitor='val_acc_0',
-        mode='max',
-        period=0
-    )
-
-    early_stopping = EarlyStopping('val_acc_0', mode='max', patience=hyper_params['patience'])
-
-    if hyper_params['precision'] not in {16, 32}:
-        raise ValueError('precision should be either 16 or 32')
-
-    if not args.no_model_restoring:
-        ckpt_to_resume = try_to_restore_model_weights(args.output)
-    else:
-        ckpt_to_resume = None
-        logger.info('will not try to restore previous models because --no-model-restoring')
-
-    tb_logger = loggers.TensorBoardLogger('experiment_logs')
-    for hparam in list(hyper_params):
-        tb_logger.experiment.add_text(hparam, str(hyper_params[hparam]))
-
-    trainer = pl.Trainer(
-        logger=tb_logger,
-        gpus=args.gpu,
-        distributed_backend='dp',
-        val_check_interval=args.validation_interval,
-        min_epochs=1,
-        gradient_clip_val=hyper_params['gradient_clipping'],
-        checkpoint_callback=checkpoint_callback,
-        early_stop_callback=early_stopping,
-        precision=hyper_params['precision'],
-        resume_from_checkpoint=ckpt_to_resume,
-        accumulate_grad_batches=hyper_params['accumulate_grad_batches'],
-        max_epochs=hyper_params['max_epochs'])
-
-    ret_trainee = RetrieverTrainer(ret, train_dataloader, dev_dataloaders, test_dataloader,
-                                   hyper_params['loss_type'], hyper_params['optimizer'])
+    ckpt_to_resume, ret_trainee, trainer = init_model(
+        hyper_params, args.num_workers, args.output, args.validation_interval, args.gpu,
+        args.no_model_restoring, args.debug)
 
     if args.train:
         trainer.fit(ret_trainee)
@@ -156,6 +75,81 @@ def main():
         evaluate_model(ret_trainee, qa_pairs_json_file=args.predict, predict_to=args.predict_to)
     else:
         logger.warning('please select one between --train / --validate / --test')
+
+
+def init_model(hyper_params, num_workers, output, validation_interval, gpu, no_model_restoring,
+               debug):
+    check_and_log_hp(
+        ['train_file', 'dev_files', 'test_file', 'batch_size', 'tokenizer_name',
+         'model', 'max_question_len', 'max_paragraph_len', 'patience', 'gradient_clipping',
+         'max_epochs', 'loss_type', 'optimizer', 'precision', 'accumulate_grad_batches', 'seed'],
+        hyper_params)
+    if hyper_params['seed'] is not None:
+        # fix the seed
+        torch.manual_seed(hyper_params['seed'])
+        np.random.seed(hyper_params['seed'])
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+    os.makedirs(hyper_params['cache_folder'], exist_ok=True)
+    tokenizer_name = hyper_params['tokenizer_name']
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
+    train_dataloader = generate_dataloader(
+        hyper_params['train_file'], hyper_params['max_question_len'],
+        hyper_params['max_paragraph_len'], tokenizer, hyper_params['batch_size'],
+        num_workers=num_workers, shuffle=True)
+    dev_dataloaders = []
+    for dev_file in hyper_params['dev_files'].values():
+        dev_dataloaders.append(
+            generate_dataloader(
+                dev_file,
+                hyper_params['max_question_len'],
+                hyper_params['max_paragraph_len'],
+                tokenizer, hyper_params['batch_size'],
+                num_workers=num_workers,
+                shuffle=False
+            )
+        )
+    test_dataloader = generate_dataloader(
+        hyper_params['test_file'], hyper_params['max_question_len'],
+        hyper_params['max_paragraph_len'], tokenizer, hyper_params['batch_size'],
+        num_workers=num_workers, shuffle=False)
+    ret = load_model(hyper_params, tokenizer, debug)
+    os.makedirs(output, exist_ok=True)
+    checkpoint_callback = ModelCheckpoint(
+        filepath=os.path.join(output, '{epoch}-{val_acc_0:.2f}-{val_loss_0:.2f}'),
+        save_top_k=1,
+        verbose=True,
+        monitor='val_acc_0',
+        mode='max',
+        period=0
+    )
+    early_stopping = EarlyStopping('val_acc_0', mode='max', patience=hyper_params['patience'])
+    if hyper_params['precision'] not in {16, 32}:
+        raise ValueError('precision should be either 16 or 32')
+    if not no_model_restoring:
+        ckpt_to_resume = try_to_restore_model_weights(output)
+    else:
+        ckpt_to_resume = None
+        logger.info('will not try to restore previous models because --no-model-restoring')
+    tb_logger = loggers.TensorBoardLogger('experiment_logs')
+    for hparam in list(hyper_params):
+        tb_logger.experiment.add_text(hparam, str(hyper_params[hparam]))
+    trainer = pl.Trainer(
+        logger=tb_logger,
+        gpus=gpu,
+        distributed_backend='dp',
+        val_check_interval=validation_interval,
+        min_epochs=1,
+        gradient_clip_val=hyper_params['gradient_clipping'],
+        checkpoint_callback=checkpoint_callback,
+        early_stop_callback=early_stopping,
+        precision=hyper_params['precision'],
+        resume_from_checkpoint=ckpt_to_resume,
+        accumulate_grad_batches=hyper_params['accumulate_grad_batches'],
+        max_epochs=hyper_params['max_epochs'])
+    ret_trainee = RetrieverTrainer(ret, train_dataloader, dev_dataloaders, test_dataloader,
+                                   hyper_params['loss_type'], hyper_params['optimizer'])
+    return ckpt_to_resume, ret_trainee, trainer
 
 
 if __name__ == '__main__':
