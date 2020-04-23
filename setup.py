a1377d8ee3bde5d209e2c9b65a5dc2ac7c93257c
Mirko Bronzi <m.bronzi@gmail.com>
2020-04-23 12:30:00 -0400

    changed version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bert_reranker',
-    version='0.0.1',
+    version='0.1.0',
     packages=find_packages(include=['bert_reranker', 'bert_reranker.*']),
     python_requires='>=3.7',
     install_requires=[
