b459657e45885a9abe7f81f78d6ce5adc994b1fa
Mirko Bronzi <m.bronzi@gmail.com>
2020-04-29 11:21:38 -0400

    can generate healthtap dataset with more options (#46)
    * can generate healthtap dataset with more options
    
    * compelted refactoring
    
    * compelted refactoring
    
    * added missing dependency

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     url='https://github.com/mirkobronzi/bert_reranker',
     python_requires='>=3.7',
     install_requires=[
-        'flake8', 'tqdm', 'pyyaml>=5.3', 'pytest', 'numpy>=1.16.4', 'pytest',
+        'flake8', 'tqdm', 'pyyaml>=5.3', 'pytest', 'numpy>=1.16.4', 'pytest', 'pandas',
         'torch', 'transformers', 'pytorch-lightning', 'sklearn', 'pandas', 'setuptools>=41.0.0'],
     entry_points={
         'console_scripts': [
