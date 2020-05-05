f65f9152dca149db7f3c7f465f6f29a4c41ffb53
mirkobronzi <m.bronzi@gmail.com>
2020-05-05 10:18:12 -0400

    now supporting more files

--- bert_reranker/scripts/combine_questions_and_answers.py
+++ bert_reranker/scripts/combine_questions_and_answers.py
@@ -1,15 +1,17 @@
 import argparse
 import json
 import logging
+import ntpath
+import os
 
 logger = logging.getLogger(__name__)
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input-questions", required=True)
+    parser.add_argument("--input-questions", required=True, nargs='+')
     parser.add_argument("--input-candidates", required=True)
-    parser.add_argument("--output", help="output json", required=True)
+    parser.add_argument("--output", help="output folder", required=True)
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.INFO)
@@ -17,16 +19,21 @@ def main():
     with open(args.input_candidates, "r", encoding="utf-8") as in_stream:
         candidates = [x. strip() for x in in_stream.readlines()]
 
+    for input_question in args.input_questions:
+        _generate_json(input_question, os.path.join(args.output,
+                                                    ntpath.basename(input_question)), candidates)
+
+
+def _generate_json(input_file, output_file, candidates):
     qa_pairs = []
-    with open(args.input_questions, "r", encoding="utf-8") as in_stream:
+    with open(input_file, "r", encoding="utf-8") as in_stream:
         for question in in_stream:
             qa_pair = [question.strip(), candidates]
             qa_pairs.append(qa_pair)
-
-    with open(args.output, 'w', encoding="utf-8") as out_stream:
+    with open(output_file, 'w', encoding="utf-8") as out_stream:
         json.dump(qa_pairs, out_stream, indent=4, ensure_ascii=False)
-
-    logger.info('used {} candidates and {} questions'.format(len(candidates), len(qa_pairs)))
+    logger.info('file {}: used {} candidates and {} questions'.format(
+        output_file, len(candidates), len(qa_pairs)))
 
 
 if __name__ == "__main__":
