83c2c2cf86f9e9f990415f28c322a8e6cedc4296
mirkobronzi <m.bronzi@gmail.com>
2020-05-05 15:19:07 -0400

    improved prediction output

--- bert_reranker/data/predict.py
+++ bert_reranker/data/predict.py
@@ -13,7 +13,7 @@ def get_batched_pairs(qa_pairs, batch_size):
     return result
 
 
-def evaluate_model(ret_trainee, qa_pairs_json_file, predict_to):
+def evaluate_model(ret_trainee, qa_pairs_json_file, predict_to, ground_truth_available):
 
     with open(qa_pairs_json_file, "r", encoding="utf-8") as f:
         qa_pairs = json.load(f)
@@ -21,6 +21,9 @@ def evaluate_model(ret_trainee, qa_pairs_json_file, predict_to):
     correct = 0
     count = 0
     out_stream = open(predict_to, 'w') if predict_to else None
+
+    if not ground_truth_available:
+        out_stream.write('!!NO GROUND TRUTH AVAILABLE FOR THESE PREDICTIONS!!\n\n')
     for question, answers in tqdm(qa_pairs):
 
         out = ret_trainee.retriever.predict(question, answers)
@@ -31,15 +34,23 @@ def evaluate_model(ret_trainee, qa_pairs_json_file, predict_to):
         if out_stream:
             out_stream.write('-------------------------\n')
             out_stream.write('question:\n\t{}\n'.format(question))
-            out_stream.write(
-                'predicted answer: correct? {} / score {:3.3} / norm score {:3.3} / answer content:'
-                '\n\t{}\n'.format(
-                    out[2][0] == 0, out[1][0], out[3][0], out[0][0]))
-            out_stream.write(
-                'ground truth:\n\t{}\n\n'.format(answers[0]))
+            if ground_truth_available:
+                out_stream.write(
+                    'prediction: correct? {} / score {:3.3} / norm score {:3.3} / answer content:'
+                    '\n\t{}\n'.format(
+                        out[2][0] == 0, out[1][0], out[3][0], out[0][0]))
+                out_stream.write(
+                    'ground truth:\n\t{}\n\n'.format(answers[0]))
+            else:
+                out_stream.write(
+                    'prediction: score {:3.3} / norm score {:3.3} / answer content:'
+                    '\n\t{}\n'.format(out[1][0], out[3][0], out[0][0]))
         count += 1
 
-    acc = correct / len(qa_pairs) * 100
-    logger.info("correct {} over {} - accuracy is {}".format(correct, len(qa_pairs), acc))
+    if ground_truth_available:
+        acc = correct / len(qa_pairs) * 100
+        logger.info("correct {} over {} - accuracy is {}".format(correct, len(qa_pairs), acc))
+    else:
+        logger.info("--ground-truth-available not used - not computing accuracy")
     if out_stream:
         out_stream.close()
