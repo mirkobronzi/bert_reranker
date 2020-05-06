323750eaece110960c90d8d2a07f34c8e12f930e
mirkobronzi <m.bronzi@gmail.com>
2020-05-06 17:41:49 -0400

    added script to normalize input

--- bert_reranker/scripts/normalize_data.py
+++ bert_reranker/scripts/normalize_data.py
@@ -1,25 +1,69 @@
 import argparse
 import json
 import logging
+import re
 
-from bert_reranker.data.data_normalization import clean_text
+from nltk import word_tokenize
+from nltk.corpus import stopwords
 
 logger = logging.getLogger(__name__)
+
+
+def remove_links(s):
+    """swap a link with a _URL_ token"""
+    return re.sub(r"http\S+", "_URL_", s)
+
+
+def remove_html_tags(s):
+    """Remove html tags from a string"""
+    clean = re.compile('<.*?>')
+    return re.sub(clean, '', s)
+
+
+def remove_standard_punctuation(s):
+    punctuation = '¿!"#$%&\'()*+,-./:;<=>?@[\\]^`{|}~'  # We are leaving out _ for the _URL_ token
+    return s.translate(str.maketrans(punctuation, ' ' * len(punctuation)))
+
+
+def remove_extra_whitespace(s):
+    s = ' '.join(s.split())
+    return s
+
+
+def remove_stopwords(sentence):
+    stop_words = set(stopwords.words('english'))
+    tokens = word_tokenize(sentence)
+    filtered_tokens = [w for w in tokens if w not in stop_words]
+    filtered_sentence = ' '.join(filtered_tokens)
+    return filtered_sentence
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--input',
                         help='data file containing questions and answers', required=True)
+    parser.add_argument("--output", help="json file where to write the output", required=True)
+    parser.add_argument("--remove-urls", action='store_true')
+    parser.add_argument("--remove-stopwords", action='store_true')
     args = parser.parse_args()
     logging.basicConfig(level=logging.INFO)
 
     with open(args.input, 'r', encoding='utf-8') as in_stream:
         qa_pairs = json.load(in_stream)
 
-    for question, answer in qa_pairs:
-        cleaned_question = clean_text(question)
-        logger.info('{} => {}'.format(question, cleaned_question))
+    cleaned_qa_pqirs = []
+    for question, answers in qa_pairs:
+        if args.remove_urls:
+            answers = [remove_links(answer) for answer in answers]
+            question = remove_links(question)
+        if args.remove_stopwords:
+            answers = [remove_stopwords(answer) for answer in answers]
+            question = remove_stopwords(question)
+        cleaned_qa_pqirs.append((question, answers))
+
+    logger.info('final json contains {} examples'.format(len(qa_pairs)))
+    with open(args.output, "w", encoding="utf-8") as out_stream:
+        json.dump(cleaned_qa_pqirs, out_stream, indent=4, ensure_ascii=False)
 
 
 if __name__ == '__main__':
