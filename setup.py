e9fe7edbbf9790d359ea04a823189e317cf63cfe
mirkobronzi <m.bronzi@gmail.com>
2020-05-28 16:17:52 -0400

    new version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bert_reranker',
-    version='0.1.1',
+    version='0.2.0',
     packages=find_packages(include=['bert_reranker', 'bert_reranker.*']),
     license='MIT',
     author='Mirko Bronzi',
