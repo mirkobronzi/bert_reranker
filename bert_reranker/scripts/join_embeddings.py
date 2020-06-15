47ad4468f5b7eabb8c7c93d3252f2fc6375093e8
Mirko Bronzi <m.bronzi@gmail.com>
2020-06-15 11:39:43 -0400

    Add option to use faq (#73)
    * added more option to join embeddings
    
    * added option to not load the fine-tuned parameter
    
    * added option to use pre-trained model on generate embedding as well
    
    * fixed param type

--- bert_reranker/scripts/join_embeddings.py
+++ bert_reranker/scripts/join_embeddings.py
@@ -8,40 +8,35 @@ logger = logging.getLogger(__name__)
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--inputs", help="emb files to join", required=True, type=str, nargs='+')
+    parser.add_argument("--passages", help="emb file with passages", required=True)
     parser.add_argument("--output", required=True)
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.INFO)
 
-    if type(args.inputs) != list or len(args.inputs) < 2:
-        raise ValueError('please specify at least two input files')
-
-    with open(args.inputs[0], "rb") as in_stream:
-        data = pickle.load(in_stream)
-
-    passage_size = len(data['passage_header_embs'])
-    logger.info('first data file {} has {} passages / {} examples'.format(
-        args.inputs[0], passage_size, len(data['question_embs'])))
-
-    for input_to_merge in args.inputs[1:]:
+    merged_user_question = []
+    merged_user_labels = []
+    for input_to_merge in args.inputs:
         with open(input_to_merge, "rb") as in_stream:
             data_to_merge = pickle.load(in_stream)
-            data['question_embs'].extend(data_to_merge['question_embs'])
-            data['question_labels'].extend(data_to_merge['question_labels'])
+            merged_user_question.extend(data_to_merge['question_embs'])
+            merged_user_labels.extend(data_to_merge['question_labels'])
 
             logger.info('data file {} has {} examples'.format(
                 input_to_merge, len(data_to_merge['question_embs'])))
 
-            if passage_size != len(data_to_merge['passage_header_embs']):
-                logger.warning('file {} has {} passages - while the first file has {}'.format(
-                    input_to_merge, len(data_to_merge['passage_header_embs']), passage_size
-                ))
+    with open(args.passages, "rb") as in_stream:
+        data_passages = pickle.load(in_stream)
+
+    to_pickle = {'passage_header_embs': data_passages['passage_header_embs'],
+                 'question_embs': merged_user_question,
+                 'question_labels': merged_user_labels}
 
     logger.info('final size: {} passages / {} examples'.format(
-        len(data['passage_header_embs']), len(data['question_embs'])))
+        len(to_pickle['passage_header_embs']), len(to_pickle['question_embs'])))
 
     with open(args.output, "wb") as out_stream:
-        pickle.dump(data, out_stream)
+        pickle.dump(to_pickle, out_stream)
 
 
 if __name__ == "__main__":
