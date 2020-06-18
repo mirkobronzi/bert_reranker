01810d83704b3019b8af233b20da7865f7536922
mirkobronzi <m.bronzi@gmail.com>
2020-06-18 15:40:58 -0400

    simplified data / fixed bug when extracting last passage header

--- examples/local/run.sh
+++ examples/local/run.sh
@@ -1,2 +1,4 @@
 main --config config.yaml --output output --train --validation-interval 1.0
+
+# uncomment the following to run predictions
 #main --config config.yaml --output output --predict data_small.json --predict-to predictions.txt
