958b4bb5cc1e17ad9e14ef1058b23b340d3bc25e
mirkobronzi <m.bronzi@gmail.com>
2020-06-18 15:28:22 -0400

    made the end2end test smaller

--- examples/local/run.sh
+++ examples/local/run.sh
@@ -1,2 +1,2 @@
-main --config config.yaml --output output --train
+main --config config.yaml --output output --train --validation-interval 1.0
 #main --config config.yaml --output output --predict data_small.json --predict-to predictions.txt
